a7db0a69404dfd77473a76c107dc285901109b54
szubaaleksander <szubaalek07@gmail.com>
2021-06-13 19:00:49 +0200

    DTO ChanThreads

--- backend/src/main/java/pwr/piisw/backend/controller/ChanThreadResource.java
+++ backend/src/main/java/pwr/piisw/backend/controller/ChanThreadResource.java
@@ -1,11 +1,13 @@
 package pwr.piisw.backend.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import pwr.piisw.backend.dto.ChanThreadDto;
 import pwr.piisw.backend.models.ChanThread;
 import pwr.piisw.backend.services.ChanThreadService;
 
@@ -14,25 +16,34 @@ import pwr.piisw.backend.services.ChanThreadService;
 public class ChanThreadResource {
   private final ChanThreadService chanThreadService;
 
+  @Autowired private ModelMapper modelMapper;
+
   public ChanThreadResource(ChanThreadService chanThreadService) {
     this.chanThreadService = chanThreadService;
   }
 
   @PostMapping("threads")
-  public ResponseEntity<ChanThread> saveThread(@RequestBody ChanThread chanThread) {
-    return new ResponseEntity<>(chanThreadService.saveThread(chanThread), HttpStatus.OK);
+  @ResponseBody
+  public ChanThreadDto saveThread(@RequestBody ChanThread chanThread) {
+    return convertToDto(chanThreadService.saveThread(chanThread));
   }
 
   @GetMapping("/threads/{id}")
-  public ResponseEntity<ChanThread> getChanThread(@PathVariable("id") int id) {
+  @ResponseBody
+  public ChanThreadDto getChanThread(@PathVariable("id") int id) {
     ChanThread chanThread = chanThreadService.getChanThread(id);
-    return new ResponseEntity<>(chanThread, HttpStatus.OK);
+    return convertToDto(chanThread);
   }
 
   @GetMapping("threads")
-  public ResponseEntity<List<ChanThread>> getAllChanThread(
+  @ResponseBody
+  public List<ChanThreadDto> getAllChanThread(
       @RequestParam(required = false) int limit, @RequestParam(required = false) int offset) {
     List<ChanThread> allChanThreads = chanThreadService.getAllChanThreads(limit, offset);
-    return new ResponseEntity<>(allChanThreads, HttpStatus.OK);
+    return allChanThreads.stream().map(this::convertToDto).collect(Collectors.toList());
+  }
+
+  private ChanThreadDto convertToDto(ChanThread chanThread) {
+    return modelMapper.map(chanThread, ChanThreadDto.class);
   }
 }
